c3ebc1e7b793eafe9f183d0b62a5beab9fb85f8d
Joshua Burbidge <joshburbidge13@gmail.com>
2024-12-22 12:13:37 -0600

    change title

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ mod helpers;
 
 fn main() {
     #[cfg(not(target_arch = "wasm32"))]
-    helpers::start(1000, 600, "my demo");
+    helpers::start(1000, 600, "femtovg app");
     #[cfg(target_arch = "wasm32")]
     helpers::start();
 }
