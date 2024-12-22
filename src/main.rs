818e0e0d88c056452408b5aa6d3a1f215120682f
Joshua Burbidge <joshburbidge13@gmail.com>
2024-12-22 12:03:34 -0600

    opengl use shared generic app struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::sync::Arc;
+
+use femtovg::{Canvas, Color, Paint, Path};
+use helpers::WindowSurface;
+use winit::{
+    application::ApplicationHandler,
+    event::{ElementState, MouseButton, WindowEvent},
+    event_loop::ActiveEventLoop,
+    window::{Window, WindowId},
+};
+
 mod helpers;
 
 fn main() {
@@ -5,4 +16,123 @@ fn main() {
     helpers::start(1000, 600, "my demo", true);
     #[cfg(target_arch = "wasm32")]
     helpers::start();
+}
+
+pub struct App<W: WindowSurface> {
+    mousex: f32,
+    mousey: f32,
+    dragging: bool,
+    window: Arc<Window>,
+    canvas: Canvas<W::Renderer>,
+    surface: W,
+}
+impl<W: WindowSurface> App<W> {
+    fn new(canvas: Canvas<W::Renderer>, surface: W, window: Arc<Window>) -> Self {
+        App {
+            canvas,
+            surface,
+            window,
+            mousex: 0.,
+            mousey: 0.,
+            dragging: false,
+        }
+    }
+}
+impl<W: WindowSurface> ApplicationHandler for App<W> {
+    fn resumed(&mut self, _event_loop: &ActiveEventLoop) {}
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            #[cfg(not(target_arch = "wasm32"))]
+            WindowEvent::Resized(physical_size) => {
+                let surface = &mut self.surface;
+
+                surface.resize(physical_size.width, physical_size.height);
+            }
+            WindowEvent::CursorMoved {
+                device_id: _,
+                position,
+                ..
+            } => {
+                let canvas = &mut self.canvas;
+
+                if self.dragging {
+                    let p0 = canvas
+                        .transform()
+                        .inverse()
+                        .transform_point(self.mousex, self.mousey);
+                    let p1 = canvas
+                        .transform()
+                        .inverse()
+                        .transform_point(position.x as f32, position.y as f32);
+
+                    canvas.translate(p1.0 - p0.0, p1.1 - p0.1);
+
+                    self.window.request_redraw();
+                }
+
+                self.mousex = position.x as f32;
+                self.mousey = position.y as f32;
+            }
+            WindowEvent::MouseWheel {
+                device_id: _,
+                delta: winit::event::MouseScrollDelta::LineDelta(_, y),
+                ..
+            } => {
+                // it's a PixelDelta in wasm
+                let canvas = &mut self.canvas;
+
+                let pt = canvas
+                    .transform()
+                    .inverse()
+                    .transform_point(self.mousex, self.mousey);
+                canvas.translate(pt.0, pt.1);
+                canvas.scale(1.0 + (y / 10.0), 1.0 + (y / 10.0));
+                canvas.translate(-pt.0, -pt.1);
+
+                self.window.request_redraw();
+            }
+            WindowEvent::MouseInput {
+                button: MouseButton::Left,
+                state,
+                ..
+            } => match state {
+                ElementState::Pressed => self.dragging = true,
+                ElementState::Released => self.dragging = false,
+            },
+            WindowEvent::RedrawRequested { .. } => {
+                let window = &self.window;
+                let canvas = &mut self.canvas;
+                let surface = &mut self.surface;
+
+                let size = window.inner_size();
+                let dpi_factor = window.scale_factor();
+
+                canvas.set_size(size.width, size.height, dpi_factor as f32);
+                canvas.clear_rect(0, 0, size.width, size.height, Color::black());
+
+                let mut path = Path::new();
+                path.move_to(0., 0.);
+                path.line_to(100., 100.);
+                canvas.stroke_path(&path, &Paint::color(Color::white()));
+
+                surface.present(canvas);
+                // this is calling flush_to_surface and swap_buffers
+            }
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            // _ => {}
+            _ => {
+                println!("{:?}", event);
+                #[cfg(target_arch = "wasm32")]
+                web_sys::console::log_1(&format!("{:?}", event).into());
+            }
+        }
+    }
 }
